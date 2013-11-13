d2a920739277919f7c1f733bdb7216184d98a83b
Ryan Aprill <ryan.aprill@gmail.com>
2013-11-13 12:43:31 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Betahire::Application.config.secret_key_base = 'fb5d882dc8f1a52ce9fe94dfa32170522b3ac89e4b89c0c7cdc56cf67b2d2225daaad0af3fbd6743970d4738b350fa73dae3c4b2a37d69199ac5d23f8530f037'
