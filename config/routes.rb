6d3de8828e809bf1d3d4eb92c3b7ad8295207758
Ryan Aprill <ryan.aprill@gmail.com>
2013-11-22 15:19:41 -0600

    adding profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Betahire::Application.routes.draw do
+  resources :profiles
   devise_for :views
   devise_for :users, :controllers => { :registrations => "registrations" }
   root "pages#home" 
   get  "about" => "pages#about"
-  get  "profile/edit" => "profile#edit"
+  match "profile" => "profiles#show", via: :get
 end
